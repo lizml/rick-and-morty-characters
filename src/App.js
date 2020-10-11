1e5740c6fa1cff243cb9c31f4efdbe927855f5b2
LIZ <liz@MacBook-Air-de-LIZ.local>
2020-10-11 15:27:05 -0500

    nav-var and component added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router,
+  Route, Switch,
+} from 'react-router-dom'
+ 
+import Navigation from './components/Navigation';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+       <Navigation/>
+       <Switch>
+        <Route path="/characters"component={Characters}/>
+        <Route path="/locations"component={Locations}/>
+        <Route path="/"component={Home}/>
+       </Switch>
+        
+    </Router>
+    
   );
 }
+
+  
+  function Home(){
+    return (
+      <div className="container home">
+        <h2>Rick and Morty Router</h2>
+        <div className="poster">
+          <img alt="rickandmorty router" src="https://cdn.custom-cursor.com/collections/rick_and_morty.png"/>
+        </div>
+        </div>
+    )
+  }
+
+  function Characters(){
+    return (
+    <h2>Characters</h2>
+    )
+  }
+  function Locations(){
+    return (
+    <h2>Locations</h2>
+    )
+  }
+
+
 
 export default App;
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import{NavLink} from 'react-router-dom';
+
+function Navigation(){
+    return(
+      <nav className= "black">
+        <ul>
+          <li className= "yellow"> 
+            <NavLink exact to="/"> Home </NavLink>
+            </li>
+          <li className="yellow">
+            <NavLink to = "/characters"> Characters</NavLink>
+         </li>
+          <li className="yellow"><NavLink to ="/locations"> Locations</NavLink>
+           </li>
+        </ul>
+      </nav>
+    )
+  }
+
+  export default Navigation;
